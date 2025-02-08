1a326d5a6c726f9fc78c7f058c9020b845a1e093
Anwar Work <Anwaralbana11111@gmail.com>
2025-02-08 17:17:41 +0300

    Update the main screen (Function )

--- Bank_inOPP_inCPP.cpp
+++ Bank_inOPP_inCPP.cpp
@@ -1,57 +1,10 @@
 ﻿
 #include <iostream>
-#include "clsBankClient.h"
-#include "clsInputValidate.h"
 #include "clsMainScreen.h"
 using namespace std;
-
-void ReadClientInfo(clsBankClient& Client) {
-    cout << "\nFirstName is :";
-    Client.FirstName = clsInputValidate::ReadString();
-
-    cout << "\nLastName is :";
-    Client.LastName = clsInputValidate::ReadString();
-
-    cout << "\nPhone is :";
-    Client.Phone = clsInputValidate::ReadString();
-
-    cout << "\nEmail is :";
-    Client.Email = clsInputValidate::ReadString();
-
-    cout << "\nPinCode is :";
-    Client.PinCode = clsInputValidate::ReadString();
-
-    cout << "\nAccount Balance is :";
-    Client.AccountBalance = clsInputValidate::ReadDblNumber();
-}
-
-void UpdateClient() {
-
-
-    string AccountNumber = "";
-    cout << "Please Enter Account to Clinet : ";
-    AccountNumber = clsInputValidate::ReadString();
-    while (!clsBankClient::IsClientExist(AccountNumber)) {
-        cout << "the AccountNubmer is not found, Choose anther one: ";
-        AccountNumber = clsInputValidate::ReadString();
-    }
-    clsBankClient Client = clsBankClient::Find(AccountNumber);
-    Client.Print();
-    cout << "\n___ Update Clietn ___\n";
-    cout << "_____________________\n";
-    ReadClientInfo(Client);
-    cout << "_____________________\n";
-
-
-}
-
-
-
-
 int main()
 {
     clsMainScreen::ShowMainMenue();
     system("pause>0");
     return 0;
 }
-
